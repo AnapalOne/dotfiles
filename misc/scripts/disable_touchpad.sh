1f0edbc39baf5b263773f7ee489f0cd9bf91de2e
Anapal <91709385+AnapalOne@users.noreply.github.com>
2022-11-26 01:10:26 +0800

    Add disable and enable touchpad sciripts

---
+++ misc/scripts/disable_touchpad.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+xinput disable 11
+notify-send -a "touchpad toggle" "Touchpad disabled." -h string:x-canonical-private-synchronous:my-notification
---
+++ misc/scripts/enable_touchpad.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+xinput enable 11
+notify-send -a "touchpad toggle" "Touchpad enabled." -h string:x-canonical-private-synchronous:my-notification
