9193e09fd4dd0fed81050606f9399750bc5652c5
Anapal <91709385+AnapalOne@users.noreply.github.com>
2023-09-20 08:30:32 +0800

    chore: remove duplicate scripts

--- scripts/disable_touchpad.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-xinput disable 11
-notify-send -a "touchpad toggle" "Touchpad disabled." -h string:x-canonical-private-synchronous:my-notification
--- scripts/enable_touchpad.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-xinput enable 11
-notify-send -a "touchpad toggle" "Touchpad enabled." -h string:x-canonical-private-synchronous:my-notification
