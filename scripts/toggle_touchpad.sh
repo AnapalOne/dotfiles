046c1ac65fc526c5c0c8347e97b93fd05cb9a748
Anapal <91709385+AnapalOne@users.noreply.github.com>
2024-05-07 11:04:19 +0800

    refactor: point touchpad id into adaptable variable

--- scripts/toggle_touchpad.sh
+++ scripts/toggle_touchpad.sh
@@ -1,9 +1,27 @@
-#!/bin/sh
+#!/bin/bash
 
-if xinput list-props 14 | grep "Device Enabled (165):.*1" >/dev/null; then
-    xinput disable 14
-    notify-send -a "touchpad toggle" "Touchpad disabled." -h string:x-canonical-private-synchronous:my-notification
-else
-    xinput enable 14
-    notify-send -a "touchpad toggle" "Touchpad enabled." -h string:x-canonical-private-synchronous:my-notification
-fi
+touchpad_id=$(xinput | grep "Touchpad" | sed -e 's/^.*=//' | sed -e 's/[ \t].*//')
+touchpad_enabled=$(xinput list-props $touchpad_id | grep "Device Enabled" | sed -e 's/^.*:\t//')
+
+case $1 in
+    "-enable")
+        xinput enable $touchpad_id
+        notify-send -a "touchpad toggle" "Touchpad enabled." -h string:x-canonical-private-synchronous:my-notification
+        ;;
+    "-disable")
+        xinput disable $touchpad_id
+        notify-send -a "touchpad toggle" "Touchpad disabled." -h string:x-canonical-private-synchronous:my-notification
+        ;;
+esac
+
+
+
+#if [[ $touchpad_enabled -eq 0 ]] || [[ $1 == "-enable" ]]; then
+#    xinput enable $touchpad_id
+#    notify-send -a "touchpad toggle" "Touchpad enabled." -h string:x-canonical-private-synchronous:my-notification
+#fi
+
+#if [[ $touchpad_enabled -eq 1 ]] || [[ $1 == "-disable" ]]; then
+#    xinput disable $touchpad_id
+#    notify-send -a "touchpad toggle" "Touchpad disabled." -h string:x-canonical-private-synchronous:my-notification
+#fi
