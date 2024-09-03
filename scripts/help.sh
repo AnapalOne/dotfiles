053f0befae9e53aec24840bf09170a0e328d1569
Anapal <91709385+AnapalOne@users.noreply.github.com>
2024-09-03 13:39:31 +0800

    Show edited help message after edit

--- scripts/help.sh
+++ scripts/help.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 
-#substitute colors with ANSI color codes
-help="$(
-	cat $HOME/.config/xmonad/help |
-	sed -e 's/\$clear\$/\\033\[0m/g' \
-	    -e 's/\$cyan\$/\\033\[36;1m/g' \
-		-e 's/\$orange\$/\\033\[34;1m/g' \
-		-e 's/\$red\$/\\033\[31;1m/g' \
-		-e 's/\$white\$/\\033\[37;1m/g' \
-		-e 's/\$yellow\$/\\033\[33;1m/g'
-)"
+print_help () {
+    help="$(cat $HOME/.config/xmonad/help | 
+            sed -e 's/\$clear\$/\\033\[0m/g' \
+                -e 's/\$cyan\$/\\033\[36;1m/g' \
+                -e 's/\$orange\$/\\033\[34;1m/g' \
+                -e 's/\$red\$/\\033\[31;1m/g' \
+                -e 's/\$white\$/\\033\[37;1m/g' \
+                -e 's/\$yellow\$/\\033\[33;1m/g' \
+                -e 's/\$blue\$/\\033\[35;1m/g' )"
+    printf "$help"
+    printf '\033[?25l'
+    read -sp "" prompt
 
-printf "$help"
-printf '\033[?25l'
-read -sp "" prompt
+    if [[ $prompt == 'e' ]]; then
+        vim $HOME/.config/xmonad/help
+        print_help
+    fi
+}
 
-if [[ $prompt == 'e' ]]; then
-	vim $HOME/.config/xmonad/help
-fi
-
+print_help
 exit
